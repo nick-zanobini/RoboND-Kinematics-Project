6eca9579934af259e66a90f91ec086527e935500
Nick Zanobini <nick-zanobini@users.noreply.github.com>
2017-07-13 02:39:50 -0700

    feat: IK works but not arm not gripping cylinder

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -50,25 +50,69 @@ def handle_calculate_IK(req):
             # Calculate joint angles using Geometric IK method
             P_EE = Matrix([[px], [py], [pz]])
             R0_6 = R0_6_sym.evalf(subs={r: roll, p: pitch, y: yaw})
+            R0_6 = R0_6[0:3, 0:3] * R_corr
 
             # Calculate wrist center
-            P_WC = simplify(P_EE - (0.303) * R0_6[0:3, 0:3] * R_corr * Matrix([[0], [0], [1]]))
+            P_WC = simplify(P_EE - (0.303) * R0_6 * Matrix([[0], [0], [1]]))
 
-            # Calculate theta1, theta2 and theta3
-            theta1, theta2, theta3 = calculateTheta1_2_3(s, P_WC)
+            # Calculate theta1
+            theta1 = N(atan2(P_WC[1], P_WC[0]))
+
+            # Calculate theta3
+            P0_2 = P_2_sym.evalf(subs={q1: theta1})
+            x_c, y_c, z_c = P_WC - P0_2[0:3, :]
+
+            # link lengths
+            l_23 = a2
+            l_35 = sqrt(a3**2 + d4**2)
+            l_25 = sqrt(x_c**2 + y_c**2 + z_c**2)
+
+            # Intermediate angles for theta3
+            theta_31 = atan2(a3, d4)
+            D1 = (l_25**2 - l_23**2 - l_35**2) / (2 * l_23 * l_35)
+            theta_32 = atan2(sqrt(1 - D1**2), D1)
+            theta3 = N((theta_32 + theta_31 - (pi / 2)).subs(s))
+
+            # Calculate theta2
+            D2 = (l_23**2 - l_35**2 + l_25**2) / (2 * l_23 * l_25)
+            theta_21 = atan2(sqrt(1 - D2**2), D2)
+            theta_22 = atan2(z_c, sqrt(x_c**2 + y_c**2))
+            theta2 = N(((pi / 2) - theta_21 - theta_22).subs(s))
 
             R0_3_inv = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
-            R3_6 = np.array(R0_3_inv * R0_6 * R_corr).astype(np.float64)
+            R3_6 = np.array(R0_3_inv * R0_6).astype(np.float64)
 
-            # Calculate theta4, theta5 and theta6
-            theta4, theta5, theta6 = rotationMatrixToEulerAngles(R3_6)
+            # Individual Rotation Matrix elements for theta4 theta5 and theta6 calculations
+            r12, r13 = R3_6[0, 1], R3_6[0, 2]
+            r21, r22, r23 = R3_6[1, 0], R3_6[1, 1], R3_6[1, 2]
+            r32, r33 = R3_6[2, 1], R3_6[2, 2]
+
+            if np.abs(r23) is not 1:
+                theta5 = atan2(sqrt(r13**2 + r33**2), r23)
+                if sin(theta5) < 0:
+                    theta4 = atan2(-r33, r13)
+                    theta6 = atan2(r22, -r21)
+                else:
+                    theta4 = atan2(r33, -r13)
+                    theta6 = atan2(-r22, r21)
+            else:
+                if r23 == 1:
+                    theta5 = 0
+                    theta4 = -theta6 + atan2(-r12, -r32)
+                else:
+                    theta5 = 0
+                    theta4 = theta6 - atan2(r12, -r32)
+
+            theta1, theta2, theta3 = np.float64(theta1), np.float64(theta2), np.float64(theta3)
+            theta4, theta5, theta6 = np.float64(theta4), np.float64(theta5), np.float64(theta6)
+
             print("(px, py, pz): ({:.4f}, {:.4f}, {:.4f})".format(px, py, pz))
             print("(roll, pitch, yaw): ({:.4f}, {:.4f}, {:.4f})".format(roll, pitch, yaw))
             print(u'\u03B8'"1 = {:.4f}, " u'\u03B8'"2 = {:.4f}, " u'\u03B8'"3 = {:.4f}\n" u'\u03B8'"4 = {:.4f}, " u'\u03B8'"5 = {:.4f}, " u'\u03B8'"6 = {:.4f}\n".format(theta1, theta2, theta3, theta4, theta5, theta6))
 
         # Populate response for the IK request
         # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-        joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+        joint_trajectory_point.positions = [np.float64(theta1), np.float64(theta2), np.float64(theta3), np.float64(theta4), np.float64(theta5), np.float64(theta6)]
         joint_trajectory_list.append(joint_trajectory_point)
         # print("Calculated all thetas")
 
--- kuka_arm/scripts/IK_variables.py
+++ kuka_arm/scripts/IK_variables.py
@@ -18,116 +18,40 @@ r, p, y = symbols("r, p, y")
 rad2deg = 180. / np.pi
 
 # Define Modified DH Transformation matrix
-s = {alpha0: 0, a0: 0, d1: 0.75,
-     alpha1: -np.pi / 2, a1: 0.35, d2: 0, q2: q2 - np.pi / 2,
-     alpha2: 0, a2: 1.25, d3: 0,
-     alpha3: -np.pi / 2, a3: -0.054, d4: 1.50,
-     alpha4: np.pi / 2, a4: 0, d5: 0,
-     alpha5: -np.pi / 2, a5: 0, d6: 0,
-     alpha6: 0, a6: 0, d7: 0.303, q7: 0
-     }
+s = {alpha0: 0., a0: 0., d1: 0.75,
+     alpha1: -pi / 2, a1: 0.35, d2: 0., q2: q2 - pi / 2.,
+     alpha2: 0., a2: 1.25, d3: 0.,
+     alpha3: -pi / 2, a3: -0.054, d4: 1.5,
+     alpha4: pi / 2, a4: 0., d5: 0.,
+     alpha5: -pi / 2, a5: 0., d6: 0.,
+     alpha6: 0., a6: 0., d7: 0.303, q7: 0.}
 
 
+# Rotation matrix along x axis
 def rot_x(q):
-    M1 = Matrix([[0], [0], [0]])
-    M2 = Matrix([[0, 0, 0, 1]])
-    Rx = Matrix([[1, 0, 0],
-                 [0, cos(q), -sin(q)],
-                 [0, sin(q), cos(q)]])
-    rot_x = Rx.row_join(M1)
-    rot_x = rot_x.col_join(M2)
-    return rot_x
-
-
-def rot_y(q):
-    M1 = Matrix([[0], [0], [0]])
-    M2 = Matrix([[0, 0, 0, 1]])
-    Ry = Matrix([[cos(q), 0, sin(q)],
-                 [0, 1, 0],
-                 [-sin(q), 0, cos(q)]])
-    rot_y = Ry.row_join(M1)
-    rot_y = rot_y.col_join(M2)
-    return rot_y
-
-
-def rot_z(q):
-    M1 = Matrix([[0], [0], [0]])
-    M2 = Matrix([[0, 0, 0, 1]])
-    Rz = Matrix([[cos(q), -sin(q), 0],
-                 [sin(q), cos(q), 0],
-                 [0, 0, 1]])
-    rot_z = Rz.row_join(M1)
-    rot_z = rot_z.col_join(M2)
-    return rot_z
-
-
-def trans_x(d):
-    M1 = Matrix(np.eye(3))
-    M2 = Matrix([[d], [0], [0]])
-    M3 = Matrix([[0, 0, 0, 1]])
-    Trans_x = M1.row_join(M2)
-    Trans_x = Trans_x.col_join(M3)
-    return Trans_x
-
-
-def trans_y(d):
-    M1 = Matrix(np.eye(3))
-    M2 = Matrix([[0], [d], [0]])
-    M3 = Matrix([[0, 0, 0, 1]])
-    Trans_y = M1.row_join(M2)
-    Trans_y = Trans_y.col_join(M3)
-    return Trans_y
-
-
-def trans_z(d):
-    M1 = Matrix(np.eye(3))
-    M2 = Matrix([[0], [0], [d]])
-    M3 = Matrix([[0, 0, 0, 1]])
-    Trans_z = M1.row_join(M2)
-    Trans_z = Trans_z.col_join(M3)
-    return Trans_z
-
-
-def rot_roll(q):
     R_x = Matrix([[1, 0, 0],
                   [0, cos(q), -sin(q)],
                   [0, sin(q), cos(q)]])
+
     return R_x
 
 
-def rot_pitch(q):
+# Rotation matrix along y axis
+def rot_y(q):
     R_y = Matrix([[cos(q), 0, sin(q)],
                   [0, 1, 0],
                   [-sin(q), 0, cos(q)]])
+
     return R_y
 
 
-def rot_yaw(q):
+# Rotation matrix along z axis
+def rot_z(q):
     R_z = Matrix([[cos(q), -sin(q), 0],
                   [sin(q), cos(q), 0],
                   [0, 0, 1]])
+
     return R_z
-
-
-# Calculates rotation matrix to euler angles
-# The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
-def rotationMatrixToEulerAngles(R):
-
-    sy = sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
-
-    singular = sy < 1e-6
-
-    if not singular:
-        theta4 = atan2(R[2, 1], R[2, 2])
-        theta5 = atan2(-R[2, 0], sy)
-        theta6 = atan2(R[1, 0], R[0, 0])
-    else:
-        theta4 = atan2(-R[1, 2], R[1, 1])
-        theta5 = atan2(-R[2, 0], sy)
-        theta6 = 0
-
-    return np.array([np.float64(theta4), np.clip(np.float64(theta5), -2, 2), np.float64(theta6)])
 
 
 def calculateTheta1_2_3(s, P_WC):
@@ -183,50 +107,12 @@ T2_3 = Matrix([[cos(q3), -sin(q3), 0, a2],
 
 T2_3 = T2_3.subs(s)
 
-# link3 to link4
-T3_4 = Matrix([[cos(q4), -sin(q4), 0, a3],
-               [sin(q4) * cos(alpha3), cos(q4) * cos(alpha3), -sin(alpha3), -sin(alpha3) * d4],
-               [sin(q4) * sin(alpha3), cos(q4) * sin(alpha3), cos(alpha3), cos(alpha3) * d4],
-               [0, 0, 0, 1]])
-
-T3_4 = T3_4.subs(s)
-
-# link4 to link5
-T4_5 = Matrix([[cos(q5), -sin(q5), 0, a4],
-               [sin(q5) * cos(alpha4), cos(q5) * cos(alpha4), -sin(alpha4), -sin(alpha4) * d5],
-               [sin(q5) * sin(alpha4), cos(q5) * sin(alpha4), cos(alpha4), cos(alpha4) * d5],
-               [0, 0, 0, 1]])
-
-T4_5 = T4_5.subs(s)
-
-# link5 to link6
-T5_6 = Matrix([[cos(q6), -sin(q6), 0, a5],
-               [sin(q6) * cos(alpha5), cos(q6) * cos(alpha5), -sin(alpha5), -sin(alpha5) * d6],
-               [sin(q6) * sin(alpha5), cos(q6) * sin(alpha5), cos(alpha5), cos(alpha5) * d6],
-               [0, 0, 0, 1]])
-
-T5_6 = T5_6.subs(s)
-
-# link6 to end-effector
-T6_7 = Matrix([[cos(q7), -sin(q7), 0, a6],
-               [sin(q7) * cos(alpha6), cos(q7) * cos(alpha6), -sin(alpha6), -sin(alpha6) * d7],
-               [sin(q7) * sin(alpha6), cos(q7) * sin(alpha6), cos(alpha6), cos(alpha6) * d7],
-               [0, 0, 0, 1]])
-
-T6_7 = T6_7.subs(s)
-
 # Create individual transformation matrices
 T0_2 = simplify(T0_1 * T1_2)
 T0_3 = simplify(T0_2 * T2_3)
-T0_4 = simplify(T0_3 * T3_4)
-T0_5 = simplify(T0_4 * T4_5)
-T0_6 = simplify(T0_5 * T5_6)
-T0_G = simplify(T0_6 * T6_7)
 
+# Correction Rotation Matrix
 R_corr = rot_z(pi) * rot_y(-pi / 2)
-T_total = simplify(T0_G * R_corr)
-
-R0_6_sym = simplify(rot_yaw(y) * rot_pitch(p) * rot_roll(r))
-R_corr = rot_yaw(pi) * rot_pitch(-pi / 2)
 P_2_sym = T0_2 * Matrix([0, 0, 0, 1])
-R0_3 = simplify(T0_3[0:3, 0:3].inv())
+R0_3 = simplify((T0_3[0:3, 0:3]).inv())
+R0_6_sym = simplify(rot_z(y) * rot_y(p) * rot_x(r))
